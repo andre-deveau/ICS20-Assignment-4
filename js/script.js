7e725df892d931c8dfc6b99d5276aaa25cfa753e
Andre <113055933+andre-deveau@users.noreply.github.com>
2022-12-07 14:44:41 -0500

    chaneg scrte

--- js/script.js
+++ js/script.js
@@ -10,8 +10,8 @@
  * Check servie worker.
  */
 if (navigator.serviceWorker) {
-  navigator.serviceWorker.register("/ICS2O-PWA-Template/sw.js", {
-    scope: "/ICS2O-PWA-Template/",
+  navigator.serviceWorker.register("/ICS20-Assignment-4/sw.js", {
+    scope: "/ICS20-Assignment-4/",
   })
 }
 
